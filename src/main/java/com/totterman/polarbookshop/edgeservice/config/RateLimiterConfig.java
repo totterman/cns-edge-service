fffa131cae29d5f9c3c3c66fff4b6f09eabe0201
Petri Totterman <pbt@in.totterman.com>
2023-10-26 21:38:48 +0300

    login

--- src/main/java/com/totterman/polarbookshop/edgeservice/config/RateLimiterConfig.java
+++ src/main/java/com/totterman/polarbookshop/edgeservice/config/RateLimiterConfig.java
@@ -5,11 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
 
+import java.security.Principal;
+
 @Configuration
 public class RateLimiterConfig {
 
     @Bean
     public KeyResolver keyResolver() {
-        return exchange -> Mono.just("anonymous");
+        return exchange -> exchange.getPrincipal()
+                .map(Principal::getName)
+                .defaultIfEmpty("anonymous");
     }
 }
---
+++ src/main/java/com/totterman/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.totterman.polarbookshop.edgeservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.server.csrf.CsrfToken;
+import org.springframework.security.web.server.csrf.XorServerCsrfTokenRequestAttributeHandler;
+import org.springframework.web.server.WebFilter;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityFilterChain(
+            ServerHttpSecurity http,
+            ReactiveClientRegistrationRepository clientRegistrationRepository) {
+        return http
+                .authorizeExchange(exchangeSpec -> exchangeSpec
+                        .pathMatchers("/", "/*.css", "/*.js", "/favicon.ico").permitAll()
+                        .pathMatchers(HttpMethod.GET, "/books/**").permitAll()
+                        .anyExchange().authenticated())
+                .exceptionHandling(exceptionHandlingSpec ->
+                        exceptionHandlingSpec.authenticationEntryPoint(
+                                new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .oauth2Login(Customizer.withDefaults())
+                .logout(logoutSpec -> logoutSpec.logoutSuccessHandler(
+                        oidcLogoutSuccessHandler(clientRegistrationRepository)))
+                .csrf(csrfSpec -> csrfSpec
+                        .csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(new XorServerCsrfTokenRequestAttributeHandler()::handle))
+                .build();
+    }
+
+    @Bean
+    ServerOAuth2AuthorizedClientRepository authorizedClientRepository() {
+        return new WebSessionServerOAuth2AuthorizedClientRepository();
+    }
+
+    @Bean
+    WebFilter csrfWebFilter() {
+        return (exchange, chain) -> {
+            exchange.getResponse()
+                    .beforeCommit(() -> Mono.defer(() -> {
+                        Mono<CsrfToken> csrfToken =
+                                exchange.getAttribute(CsrfToken.class.getName());
+                        return csrfToken != null ? csrfToken.then() : Mono.empty();
+                    }));
+            return chain.filter(exchange);
+        };
+    }
+
+    private ServerLogoutSuccessHandler oidcLogoutSuccessHandler(
+            ReactiveClientRegistrationRepository clientRegistrationRepository
+    ) {
+        var oidcLogoutSuccessHandler =
+                new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository);
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
+        return oidcLogoutSuccessHandler;
+    }
+
+}
